671036376a4a83073327929aa0dcdd447cf788eb
Chris Kang <chriskang@MacBook-Pro-3.local>
2019-07-24 09:37:34 +0800

    added logo + changed icons

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,80 +1,90 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import BookmarksScreen from '../screens/BookmarksScreen';
-import AccountScreen from '../screens/AccountScreen';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import BookmarksScreen from "../screens/BookmarksScreen";
+import AccountScreen from "../screens/AccountScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const config = Platform.select({
-  web: { headerMode: 'screen' },
-  default: {},
+  web: { headerMode: "screen" },
+  default: {}
 });
 
 const HomeStack = createStackNavigator(
   {
-    Home: HomeScreen,
+    Home: HomeScreen
   },
   config
 );
 
-
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+  tabBarLabel: "Home",
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
+    <MaterialCommunityIcons
       focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-home`
-          : 'md-home'
-      }
+      name="home"
+      color={focused ? "#9383e2" : "#cbcbcb"}
+      size={27}
     />
-  ),
+  )
 };
 
-HomeStack.path = '';
+HomeStack.path = "";
 
 const BookmarksStack = createStackNavigator(
   {
-    Bookmarks: BookmarksScreen,
+    Bookmarks: BookmarksScreen
   },
   config
 );
 
 BookmarksStack.navigationOptions = {
-  tabBarLabel: 'Bookmarks',
+  tabBarLabel: "Bookmarks",
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-bookmark' : 'md-bookmark'} />
-  ),
+    <MaterialCommunityIcons
+      focused={focused}
+      name="bookmark"
+      size={26}
+      color={focused ? "#9383e2" : "#cbcbcb"}
+    />
+  )
 };
 
-BookmarksStack.path = '';
+BookmarksStack.path = "";
 
 const AccountStack = createStackNavigator(
   {
-    Account: AccountScreen,
+    Account: AccountScreen
   },
   config
 );
 
 AccountStack.navigationOptions = {
-  tabBarLabel: 'Account',
+  tabBarLabel: "Account",
   tabBarIcon: ({ focused }) => (
-    <MaterialCommunityIcons focused={focused} name='account' size={29} color={focused ? '#4a94d5' : '#cbcbcb'} />
-  ),
+    <MaterialCommunityIcons
+      focused={focused}
+      name="account"
+      size={29}
+      color={focused ? "#9383e2" : "#cbcbcb"}
+    />
+  )
 };
 
-AccountStack.path = '';
+AccountStack.path = "";
 
 const tabNavigator = createBottomTabNavigator({
   HomeStack,
   BookmarksStack,
-  AccountStack,
+  AccountStack
 });
 
-tabNavigator.path = '';
+tabNavigator.path = "";
 
 export default tabNavigator;
